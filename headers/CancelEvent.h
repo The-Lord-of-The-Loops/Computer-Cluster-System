57059fff70ac1de4426fecd361c872562ce37901
Mohssen Elshaar <mohssen.elshaar@gmail.com>
2020-11-27 02:21:48 +0200

    fdbf

--- headers/CancelEvent.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Event.h"
-class CancelEvent : public Event{
-public:
-	CancelEvent(int AT, int ID, MasterNode* MN) : Event(AT, ID, MN) {};
-	void deletSysProcess(int ID);
-	void Execute() override;
-};
-
--- headers/PromotEvent.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Event.h"
-class PromotEvent : public Event{
-public:
-	PromotEvent(int AT, int ID, MasterNode* MN) : Event(AT, ID, MN) {};
-	void Execute() override;
-	void PromotIntertoSys(int ID);
-};
-
